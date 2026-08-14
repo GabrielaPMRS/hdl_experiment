60aca19a11bc43a0387fbe72708da7dc33905365
Gabriela Soares <109999804+GabrielaPMRS@users.noreply.github.com>
2026-08-14 10:49:22 -0300

    atualiza descrição de com smell e sem smell

--- lambda-smell-pares/vscode-pages/pagina-5.sv
+++ lambda-smell-pares/vscode-pages/pagina-5.sv
@@ -6,13 +6,19 @@ module example;
 
     int lo, med, hi;
     bit result;
+    int selected;
 
     initial begin
         lo = 20;
         med = 224;
         hi = 164;
 
-        result = lo < med < hi;
+        result = (lo < med < hi);
+
+        if (result)
+            selected = med;
+        else
+            selected = lo;
     end
 
 endmodule
